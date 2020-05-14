4c39cad59ccf827100f58927e415cad35921afdc
meekeboose <meekeboose@gmail.com>
2020-05-14 23:09:12 +0800

    body parser

--- calculator.js
+++ calculator.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function (req, res) {
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+
+  let result = num1 + num2;
+
+  // res.send('The result of the calculation is ' + result);
+  res.send(`<h1>The result is ${result}</h1>`);
 });
 
 app.listen(3000, function () {
